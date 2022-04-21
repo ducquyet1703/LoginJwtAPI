120d37ed6377ad067e39e70d551202dd319356c6
ducquyet1703 <gunnynhoc7@gmail.com>
2022-04-21 09:54:35 +0700

    Update Secret

--- src/main/java/com/dts/loginjwtapi/jwt/JwtTokenProvider.java
+++ src/main/java/com/dts/loginjwtapi/jwt/JwtTokenProvider.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Component
 @Slf4j
 public class JwtTokenProvider {
-    private final String JWT_SECRET = "dts";
+    private final String JWT_SECRET = "dtssssss";
     private final long JWT_EXPIRATION = 604800000L;
 
     public String generateToken(CustomUserDetails userDetails) {
